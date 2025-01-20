a70fb92235df3f4a6e0465b23e9f681ff610e3de
NayRenata <nayara_renata11@hotmail.com>
2025-01-20 20:39:54 -0300

    Atualização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Cadastro from './pages/cadastro/Cadastro'
 import './App.css'
 import Login from './pages/login/Login'
+import { AuthProvider } from './context/AuthContex'
+import ListaTemas from '../../blogpessoal/src/temas/listatemas/ListaTemas'
+import FormTema from './temas/formtema/FormTema'
+import DeletarTema from './temas/deletartema/DeletarTema'
+
 
 function App() {
     return (
         <>
+        <AuthProvider>
         <BrowserRouter>
               <Navbar/>
                 <div className="min-h-[80vh]">
@@ -17,11 +23,16 @@ function App() {
                         <Route path="/home" element={<Home />} /> 
                         <Route path="/cadastro" element={<Cadastro />} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastrartema" element={<FormTema/>} />
+                        <Route path="/editartema/:id" element={<FormTema/>} />
+                        <Route path="/deletartema/:id" element={<DeletarTema />} />
                     </Routes>
                 </div>
                 
                 <Footer/>
         </BrowserRouter>   
+        </AuthProvider>
 
         </>
     )
